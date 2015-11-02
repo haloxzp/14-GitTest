a96a3fa123dc299c6bc72f9847e44137f7cd43cf
dc004 <dc004@quanjungang.local>
2015-11-02 17:25:04 +0800

    撒地方

--- 14-GitTest/ViewController.m
+++ 14-GitTest/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"1233");
-    NSLog(@"lalala权");
+    NSLog(@"lalala权你好");
 }
 
 - (void)didReceiveMemoryWarning {
